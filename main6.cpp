0d9ed0db608cb59db769dd3ef6890be14172bec0
tojijk <32127432+tojijk@users.noreply.github.com>
2018-09-23 13:44:55 +0530

    Update main6.cpp

--- main6.cpp
+++ main6.cpp
@@ -5,10 +5,3 @@
 using namespace std;
 int main(void)
 {
-    //Upload U;
-    FILE *fn;
-    string url="https://api.github.com/repos/tojijk/testing/contents/ap6.txt";
-    string agent="tojijk", user_pwd="tojijk:spell@404";
-    findNew(url, user_pwd, agent);
-    return 0;
-}ÿ
